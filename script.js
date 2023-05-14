e2db593f650223e0710bec0a8be95814b4420af6
kunc03 <baguskuncoro003@gmail.com>
2023-05-14 10:27:19 +0700

    upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Toggle class active
+
+const navbarNav = document.querySelector('.navbar-nav');
+
+// Ketika Menu diKlick
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+
+// klik diluar side bar untuk menghilangkan nav
+ const hamburger = document.querySelector('#hamburger-menu');
+
+ document.addEventListener('click', function(e) {
+    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+ });
